52e49bd0f46df13c2198dd55dfc43cf6c5879514
vgswn <iit2015038@iiita.ac.in>
2017-10-31 00:52:21 +0530

    tic_tac_toe_min_max

--- PycharmProjects/AI_Assignments/new_tic_tac_toe_min_max.py
+++ PycharmProjects/AI_Assignments/new_tic_tac_toe_min_max.py
@@ -1,25 +1,25 @@
-def score(board,me,d):
+def score(board,me,opponent,d):
     for i in range(3):
         if(board[i][0]==board[i][1] and board[i][1]==board[i][2] and board[i][2]!='_'):
             if(board[i][0]==me):
                 return 10-d
-            else:
+            elif board[i][0]==opponent:
                 return -10+d
     for i in range(3):
         if(board[0][i]==board[1][i] and board[1][i]==board[2][i]and board[2][i]!='_'):
             if(board[0][i]==me):
                 return 10-d
-            else:
+            elif board[0][i] == opponent:
                 return -10+d
     if(board[0][0]==board[1][1] and board[1][1]==board[2][2]and board[2][2]!='_'):
         if(board[0][0])==me:
             return 10-d
-        else :
+        elif board[0][0] == opponent:
             return -10+d
     if(board[0][2]==board[1][1] and board[1][1]==board[2][0]and board[2][2]!='_'):
         if(board[0][2])==me:
             return 10-d
-        else:
+        elif board[0][2] == opponent:
             return -10+d
     return  0
 
@@ -30,7 +30,7 @@ def moves(board):
                 return 1
     return 0
 def min_max(board,me,opponent,f,d):
-    x=score(board,me,d)
+    x=score(board,me,opponent,d)
     #print(x)
     if(x!=0):
         return x
@@ -47,19 +47,34 @@ def min_max(board,me,opponent,f,d):
                     board[i][j]=opponent
                     #print(board)
 
-                    new_score=min(min_max(board,me,opponent,1,d+1),new_score)
+                    new_score=min(new_score,min_max(board,me,opponent,1,d+1))
                     board[i][j]='_'
         return new_score
-    if f==1:
+    elif f==1:
         new_score=-100
         for i in range(3):
             for j in range(3):
                 if board[i][j]=='_':
                     board[i][j]=me
                     #print(board)
-                    new_score=max(min_max(board,me,opponent,0,d+1),new_score)
+                    new_score=max(new_score,min_max(board,me,opponent,0,d+1))
                     board[i][j]='_'
         return new_score
+
+def algo(board,me,opponent):
+    score_=-1500
+    x=()
+    for i in range(3):
+        for j in range(3):
+            if board[i][j]=='_':
+                board[i][j]=me
+                value=min_max(board,me,opponent,0,0)
+                board[i][j]='_'
+                #print(i,j,value)
+                if value >score_:
+                    x=(i,j)
+                    score_=value
+    return x
 
 
 
@@ -76,27 +91,5 @@ for i in range(3):
     x=input()
     for j in range(3):
         board[i].append(x[j])
-#print(board)
-#print(board[2][2])
-#print(me,opponent)
-values={}
-a=-500
-b=(100,100)
-for i in range(3):
-    for j in range(3):
-        #print(i,j)
-        if board[i][j]=='_':
-            x=(i,j)
-            #print(x)
-            board[i][j]=me
-            q=min_max(board,me,opponent,0,0)
-            #print(i,j,q)
-            if q>=a:
-                a=q
-                b=x
-            #print(x,values[x])
-
-            board[i][j]='_'
-
-print(b[0],b[1])
-#print(max(100,50))
+x=algo(board,me,opponent)
+print(x[0],x[1])
